25e6e4140c6770d2a8d91ab08822d38e05f255ac
silbetoldo <silbetoldo@hotmail.com>
2021-08-08 22:00:39 -0300

    Quarto commit

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteMap.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    val pair: Pair<String, Double> = Pair("João", 1000.00)
+    val map1= mapOf(pair)
+    map1.forEach {(k, v) -> println("Chave:$k - Valor:$v")}
+    val map2 = mapOf(
+        "Pedro" to 2500.00,
+        "Maria" to 5500.00)
+    map2.forEach {(k, v) -> println("Chave:$k - Valor:$v")}
+}
